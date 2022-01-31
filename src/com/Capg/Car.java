05ea1ea2997c4b5e93470bbe6e3a228f41e493f6
OnkarKulkarni-13 <98728051+OnkarKulkarni-13@users.noreply.github.com>
2022-01-31 11:54:11 +0530

    first commit

---
+++ src/com/Capg/Car.java
@@ -0,0 +1,15 @@
+package com.Capg;
+
+class CarDetails{
+	private String name;
+	private float model_no;
+	private int seats;
+}
+
+public class Car {
+	public static void main(String[] args) {
+		
+
+	}
+
+}
